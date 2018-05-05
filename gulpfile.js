7a3fd95e74ca904dc42aa051b9f568f6351fa6d0
Alim <Haancoock123@gmail.com>
2018-05-05 20:26:32 +0300

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp'),
-	browserSync = require('browser-sync');
+	browserSync = require('browser-sync'),
+	useref = require('gulp-useref'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify'),
+	cleanCss = require('gulp-clean-css'),
+	rimraf = require('gulp-rimraf'),
+	filter = require('gulp-filter'),
+	size = require('gulp-size'),
+	imagemin = require('gulp-imagemin');
+
+
 
 
 	gulp.task('server', function(){
@@ -22,3 +32,55 @@ var gulp = require('gulp'),
 
 
 	gulp.task('default', ['server', 'watch']);
+
+// Build
+
+	gulp.task('useref', function(){
+		return gulp.src('app/*.html')
+			.pipe(useref())
+			.pipe(gulpif('*.js', uglify()))
+			.pipe(gulpif('*.css', cleanCss()))
+			.pipe(gulp.dest('dist'));
+
+	});
+
+
+//Clear
+	gulp.task('clear', function(){
+		return gulp.src('dist', { read: false})
+		.pipe(rimraf());
+	})
+
+//Fonts 
+	gulp.task('fonts', function(){
+		gulp.src('app/fonts/**/*.*')
+		// .pipe(filter(['*.eot', '*.svg', '*.ttf', '*.woff', '*.woff2', '*.css']))
+		.pipe(gulp.dest('dist/fonts'))	
+	});
+
+//Images 
+	gulp.task('images', function(){
+		return gulp.src('app/images/*')
+		.pipe(imagemin({
+			progressive:true,
+			interlaced: true
+		}))
+		.pipe(gulp.dest('dist/images'));
+	});
+
+//Extras
+	gulp.task('extras', function(){
+		return gulp.src([
+			'app/*.*',
+			'!app/*.html'
+			]).pipe(gulp.dest('dist'));
+	})
+
+//Build 
+gulp.task('build', ['clear'], function(){
+	gulp.start('dist');
+})
+
+gulp.task('dist', ['useref', 'images', 'fonts', 'extras'], function(){
+	return gulp.src('dist/**/*').pipe(size({title: 'build'}));
+});
